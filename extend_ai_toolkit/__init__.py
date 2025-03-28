397ffb2c7cacc1ac674fee0eef9aca094331f8c3
Jonathan Bailey <jonbailey@paywithextend.com>
2025-03-28 09:54:44 -0400

    adding package exports to the toolkit so you can 'from extend_ai_toolkit import ExtendMCPServer' etc

--- extend_ai_toolkit/__init__.py
+++ extend_ai_toolkit/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "0.1.0"
+
+from .langchain import ExtendLangChainToolkit
+from .modelcontextprotocol import ExtendMCPServer, Options, validate_options
+from .openai import ExtendOpenAIToolkit
+
+__all__ = [
+    "ExtendLangChainToolkit",
+    "ExtendMCPServer",
+    "ExtendOpenAIToolkit",
+    "Options",
+    "validate_options",
+]
